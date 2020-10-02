b4c1e8cf7d9558f137d11331a2e84113779cd0df
nebojsadj <djordjevicn87@gmail.com>
2020-10-02 20:36:59 +0200

    adding startTrackingJs

--- view.js
+++ view.js
@@ -1,28 +1,3 @@
-trackingBtn.onclick = startTracking;
-cigarettesCard.onclick = cigarettesView;
-smokeCard.onclick = smokerView;
-moneyCard.onclick = moneyView;
-resetBtn.onclick = resetTracking;
-
-if (localStorage.packingCigarettes) {
-  holder1.style.display = 'block';
-  holder2.style.display = 'none';
-  holder3.style.display = 'none';
-  holder4.style.display = 'none';
-  hiThere.innerHTML = `Hi ${localStorage.userNameTracking} !`;
-  smokeCard.childNodes[5].innerHTML = 'current '+'<span id="sp">'+localStorage.counter+'</span>'+ ' cigarett';
-};
-// pocetni meni, odlazak u glavni meni i istoriju
-function startTracking() {
-  holder1.style.display = 'none';
-  if (localStorage.userNameTracking && localStorage.cigarettesAverage && localStorage.packingCigarettes) {
-    container2.style.display = 'block';
-    realAverage();
-  }else {
-    userName();
-  }
-};
-
 
 function cigarettesView() {
   container2.style.display = 'none';
@@ -42,28 +17,4 @@ function moneyView() {
   backBtn.style.display = 'block';
   moneyConsumtion.style.display = 'block';
   redCard();
-};
-
-
-// dugme za povratak u meni kartica
-backBtn.onclick = function () {
-  container2.style.display = 'block';
-  cigaretteRecordsView.style.display = 'none';
-  cigConsumtion.style.display = 'none';
-  moneyConsumtion.style.display = 'none';
-  backBtn.style.display = 'none';
-  smokeCard.childNodes[5].innerHTML = 'current '+'<span id="sp">'+localStorage.counter+'</span>'+ ' cigarett';
-};
-
-
-function previousCigar() {
-  localStorage.startSat = parseInt(sati.innerHTML);
-  localStorage.startMin = parseInt(minuti.innerHTML);
-  localStorage.startDan = parseInt(dan.innerHTML);
-  localStorage.startMesec = parseInt(mesec.innerHTML);
-  localStorage.brojac = 0;
-};
-
-function resetTracking() {
-  localStorage.clear();
 };
