f871d769467caad109bce82b03b8d2aa59344af9
nebojsadj <djordjevicn87@gmail.com>
2020-10-04 13:37:52 +0200

    added input condition

--- howManyPack.js
+++ howManyPack.js
@@ -2,14 +2,14 @@
 function howManyPack() {
   holder4.style.display = 'block';
     nextBtn3.onclick = function () {
-      if (packInput.value == '') {
+      if (packInput.value == '' || !Number.isInteger(parseInt(packInput.value))) {
           packInput.style.border = '2px solid red';
       }else {
         packInput.style.border = '2px solid black';
         holder4.style.display = 'none';
         container2.style.display = 'block';
         backToStartBtn.style.display='block';
-        localStorage.packingCigarettes = packInput.value;
+        localStorage.packingCigarettes = parseInt(packInput.value);
         localStorage.counter = 0;
         smokeCard.childNodes[5].innerHTML = `current <span id="sp">${localStorage.counter}</span> cigarett`;
         localStorage.history = JSON.stringify(historyObj);
